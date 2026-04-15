1ca6c84f7af4bb4e63a68156305880b8e6e1b8a5
jimgasser <jim.gasser71@comcast.net>
2026-04-15 10:21:35 -0700

    Update

--- tropoflex/jf_tropoflex_form_helper.js
+++ tropoflex/jf_tropoflex_form_helper.js
@@ -1,10 +1,10 @@
-const myVet = document.getElementById("veterinarian");
-const myOwner = document.getElementById("owner");
-const myAnimal = document.getElementById("animal");
+const myVet = document.getElementById("veterinarian"); // input_4
+const myOwner = document.getElementById("owner"); // input_5
+const myAnimal = document.getElementById("animal"); // input_17
 const myAnimalTypes = document.querySelectorAll('input[name="q19_animalType"]');
-const myGender = document.getElementById("gender");
-const myTissueType = document.getElementById("tissueType");
-const myDisciplines = document.getElementById("discipline");
+const myGender = document.getElementById("gender"); // input_50
+const myDisciplines = document.getElementById("disciplines"); // input_77
+const myTissueType = document.getElementById("tissueType"); // input_31
 
 const paFlowUrlVets = 'https://default2ca302b2fd4a4e6a89f3ac4c6dcf2a.85.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/d46af86d45054d7dab80b0f695c0177e/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=i_QPUg80RYYFshg0tOjPED_jbrv9CAfoN5y4zk39xks';
 const paFlowUrlOwners = 'https://default2ca302b2fd4a4e6a89f3ac4c6dcf2a.85.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/08a3dddda77c4862b22f98e7a6b7d538/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=97vClsSpKD8yuIhcSjfOWCpWTKmP_oSou0UWQBoRX4w';
